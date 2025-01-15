13c63c3ec8b20d13791483702319e67593d2b493
Ozi <ozisadiqkasai@gmail.com>
2025-01-15 10:10:03 +0100

    Global styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
+import GlobalStyles from "./components/styles/Global"
 import Hero from "./pages/hero/Hero"
 import Aura from "./pages/Aura"
 
 const App = () => {
   return (
+    <>
+    <GlobalStyles />
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Hero />}/>
       <Route path='/aura' element={<Aura />}/>
       </Routes> 
     </BrowserRouter>
+    </>
   )
 }
 
