d638017ccb2079f899789e585d9d3662f0e00b52
Ozi <ozisadiqkasai@gmail.com>
2025-01-23 12:37:15 +0100

    deploy

--- server/index.js
+++ server/index.js
@@ -1,32 +1,36 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import mongoose from 'mongoose'
-import router from './Routes/ResearchRoute.js'
-import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import router from './Routes/ResearchRoute.js';
+import cors from 'cors';
 
-const app = express()
-app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
-  }));
+  })
+);
 
+app.use('/createResearch', router);
 
-  app.use(express.static(path.resolve(__dirname, './client/dist')));
+// Serve frontend
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
-  });
-app.post('/createResearch',router)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 try {
-    await mongoose.connect(process.env.MONGO_URI)
-    app.listen(process.env.PORT,()=>{
-        console.log(`server is running on ${process.env.PORT}`)
-    })
+  await mongoose.connect(process.env.MONGO_URI);
+  app.listen(process.env.PORT, () => {
+    console.log(`server is running on ${process.env.PORT}`);
+  });
 } catch (error) {
-    console.log(error)
-    process.exit(1)
+  console.log(error);
+  process.exit(1);
 }
